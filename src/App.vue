7198befd66d745f0323e577bd43522c638be893c
unknown <CTW00987-Admin@CTW-00987.criticaltechworks.com>
2020-10-03 18:41:31 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HeaderBar />
+    <div class="main-section columns">
+      <main class="column">
+        <Heroes />
+      </main>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import HeaderBar from "@/components/header-bar";
+import Heroes from "@/components/heroes";
 
-export default Vue.extend({
+export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
-});
+  components: { HeaderBar, Heroes }
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "@/design/index.scss";
 </style>
